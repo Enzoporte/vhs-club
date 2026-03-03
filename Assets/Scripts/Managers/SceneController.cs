e5d62b8cec1f69c490f44dcec1585bfe93c2f3e6
Enzoporte <enzocantoni03@gmail.com>
2026-03-03 23:28:48 +0100

    scene controller + cambios
    no está terminado todavía

---
+++ Assets/Scripts/Managers/SceneController.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    private static SceneController instance;
+    public static SceneController Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                SetupInstance();
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private static void SetupInstance()
+    {
+        instance = FindFirstObjectByType<SceneController>();
+        if (instance == null)
+        {
+            GameObject gameObj = new GameObject();
+            gameObj.name = "SceneController";
+            instance = gameObj.AddComponent<SceneController>();
+            DontDestroyOnLoad(gameObj);
+        }
+    }
+    
+    public void LoadGameplayScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
